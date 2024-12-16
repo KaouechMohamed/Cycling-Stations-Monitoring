4dcbfb0094ae4f7980260282c1dabb41517914b5
KaouechMohamed <medkaou12@gmail.com>
2024-12-16 19:24:00 +0100

    first commit

---
+++ producer.py
@@ -0,0 +1,55 @@
+import os
+import json
+import logging
+import time
+from kafka import KafkaProducer
+import urllib.request
+from dotenv import load_dotenv
+
+
+
+logging.basicConfig(level=logging.INFO)
+
+#load the api key from env variables
+def configure():
+    load_dotenv()
+
+# kafka producer
+producer = KafkaProducer(bootstrap_servers="localhost:9092", value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+# main function of the producer
+def main():
+
+    #url with api key for requesting the stations data
+    url = f"https://api.jcdecaux.com/vls/v1/stations?apiKey={os.getenv('API_KEY')}"
+    # requesting data and send to kafka broker
+    while True:
+        response = urllib.request.urlopen(url)
+        if response : 
+            logging.info("data fetched successfully!")
+            stations_data = json.loads(response.read().decode())
+
+            for station in stations_data:
+                station_data = {
+                    'number': station['number'],
+                    'contract_name': station['contract_name'],
+                    'name': station['name'],
+                    'address': station['address'],
+                    'status': station['status'],
+                    'lattitude': station['position']['lat'],
+                    'longitude': station['position']['lng'],
+                    'total_bike_stands': station['bike_stands'],
+                    'available_bike_stands': station['available_bike_stands'],
+                    'available_bikes': station['available_bikes'],
+                    'last_update': station['last_update']
+                }
+                # Send formatted data to Kafka topic
+                producer.send("cycling_stations", station_data)
+        else:
+            logging.info("request data error")
+        logging.info("{} Produced {} station records".format(time.time(), len(stations_data)))
+        time.sleep(60)
+
+if __name__=="__main__":
+    configure()
+    main()
---
+++ stream_consumer.py
@@ -0,0 +1,147 @@
+import logging
+from cassandra.cluster import Cluster
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StructField, StringType,IntegerType, FloatType
+
+logging.basicConfig(level=logging.INFO)
+
+
+def create_keyspace(session):
+    try:
+        session.execute("""
+            CREATE KEYSPACE IF NOT EXISTS spark_streams
+            WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
+        """)
+        logging.info("Keyspace created successfully!")
+    except Exception as e:
+        logging.error(f"Error creating keyspace: {e}")
+
+
+def create_table(session):
+    try:
+        session.execute("""
+        CREATE TABLE IF NOT EXISTS spark_streams.cycling_stations (
+            number INT PRIMARY KEY,
+            contract_name TEXT,
+            name TEXT,
+            address TEXT,
+            lattitude flaot,
+            longitude flaot,
+            status TEXT,
+            total_bike_stands INT,
+            available_bike_stands INT,
+            available_bikes INT,
+            last_update TEXT);
+        """)
+        logging.info("Table created successfully!")
+    except Exception as e:
+        logging.error(f"Error creating table: {e}")
+
+
+def create_spark_connection():
+    try:
+        s_conn = SparkSession.builder \
+            .appName('SparkDataStreaming') \
+            .master("spark://spark_master:7077") \
+            .config('spark.cassandra.connection.host', 'cassandra') \
+            .getOrCreate()
+
+        s_conn.sparkContext.setLogLevel("ERROR")
+        logging.info("Spark connection created successfully!")
+        return s_conn
+    except Exception as e:
+        logging.error(f"Couldn't create the spark session due to exception {e}")
+        return None
+
+
+def connect_to_kafka(spark_conn):
+    try:
+        spark_df = spark_conn.readStream \
+            .format('kafka') \
+            .option('kafka.bootstrap.servers', 'broker:9092') \
+            .option('subscribe', 'cycling_stations') \
+            .option('startingOffsets', 'earliest') \
+            .load()
+        logging.info("Kafka dataframe created successfully")
+        return spark_df
+    except Exception as e:
+        logging.error(f"Kafka dataframe could not be created because: {e}")
+        return None
+
+
+def create_cassandra_connection():
+    try:
+        cluster = Cluster(['cassandra'])
+        cas_session = cluster.connect()
+        return cas_session
+    except Exception as e:
+        logging.error(f"Could not create Cassandra connection due to {e}")
+        return None
+
+
+def create_selection_df_from_kafka(spark_df):
+    schema = StructType([
+    StructField("number", IntegerType(), True),
+    StructField("contract_name", StringType(), True),
+    StructField("name", StringType(), True),
+    StructField("address", StringType(), True),
+    StructField("lattitude", FloatType(), True),
+    StructField("longitude", FloatType(), True),
+    StructField("status", StringType(), True),
+    StructField("total_bike_stands", StringType(), True),
+    StructField("available_bike_stands", StringType(), True),
+    StructField("available_bikes", StringType(), True),
+    StructField("last_update", StringType(), True)
+    ])
+    try:
+        sel = spark_df.selectExpr("CAST(value AS STRING)") \
+            .select(from_json(col('value'), schema).alias('data')).select("data.*")
+        return sel
+    except Exception as e:
+        logging.error(f"Error creating selection DataFrame: {e}")
+        return None
+
+
+def main():
+    # Create spark connection
+    spark_conn = create_spark_connection()
+
+    if spark_conn is not None:
+        # Connect to Kafka
+        spark_df = connect_to_kafka(spark_conn)
+
+        if spark_df is not None:
+            # Create selection DataFrame
+            selection_df = create_selection_df_from_kafka(spark_df)
+
+            if selection_df is not None:
+                # Create Cassandra connection
+                session = create_cassandra_connection()
+
+                if session is not None:
+                    create_keyspace(session)
+                    create_table(session)
+
+                    logging.info("Streaming is being started...")
+
+                    streaming_query = (selection_df.writeStream
+                                       .format("org.apache.spark.sql.cassandra")
+                                       .option('checkpointLocation', '/tmp/checkpoint')
+                                       .option('keyspace', 'spark_streams')
+                                       .option('table', 'cycling_stations')
+                                       .start())
+
+                    streaming_query.awaitTermination()
+                else:
+                    logging.error("Failed to create Cassandra session")
+            else:
+                logging.error("Failed to create selection DataFrame")
+        else:
+            logging.error("Failed to connect to Kafka")
+    else:
+        logging.error("Failed to create Spark connection")
+
+
+if __name__ == "__main__":
+    main()
